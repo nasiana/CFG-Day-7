7bc90792c07174286012f1d445420a7947b32f92
nasiana <87439662+nasiana@users.noreply.github.com>
2021-10-10 20:10:53 +0100

    Add files via upload

---
+++ Lecture.py
@@ -0,0 +1,27 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# concatonate 
+print("Cat")
+print("Cat" + " videos")
+
+print("Cat" * 3)
+print("Cat" + str(3))
+
+print("Cat".upper())
+print("Cat".lower())
+
+print("the lord of the rings".title())
+
+# String formatting
+
+oranges = 12
+cost_per_orange = 0.5
+total_cost = oranges * cost_per_orange
+output = "{} oranges costs £{}".format(oranges, total_cost)
+print(output)
+
+print(f"{oranges} oranges cost £{total_cost}")
+
